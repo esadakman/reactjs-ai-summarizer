2cff981d64906d0f23f095f2dcdf3eaedc145abb
esadakman <esadd26@gmail.com>
2023-06-04 09:18:41 +0300

    user-auth-listener, user.js

---
+++ src/context/user.js
@@ -0,0 +1,6 @@
+import { createContext } from "react" 
+import "firebase/compat/auth";
+
+const UserContext = createContext(null)
+
+export default UserContext
---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,27 @@
+import { useState, useEffect, useContext } from "react";
+import FirebaseContext from "../context/firebase";
+import "firebase/compat/auth";
+
+export default function useAuthListener() {
+  
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const { firebase } = useContext(FirebaseContext);
+  
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // we have a user... therefore we can store the user in locastorage
+        localStorage.setItem("authUser", JSON.stringify(authUser)); 
+        setUser(authUser);
+      } else {
+        // we dont have an authUser, therefore clear the localstorage
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+
+    return () => listener();
+  }, [firebase])
+
+  return { user }
+}
